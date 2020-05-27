2b33c8250f8cc612f551ff031630e327d7540993
Felipe Sere <felipesere@gmail.com>
2020-05-27 08:27:45 +0200

    Use binary to blob over all possible tf files

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,36 @@
-use tree_sitter::{Parser, Language, Query, QueryCursor, QueryMatch};
-use std::path::PathBuf;
+use glob::glob;
+use tree_sitter::{Language, Parser, Query, QueryCursor};
+use std::fs::read_to_string;
 
 fn main() {
-    extern "C" { fn tree_sitter_terraform() -> Language; }
+    extern "C" {
+        fn tree_sitter_terraform() -> Language;
+    }
 
     let mut parser = Parser::new();
     let language = unsafe { tree_sitter_terraform() };
     parser.set_language(language).unwrap();
 
-    let path_to_file: PathBuf = ["sample.tf"].iter().collect();
-    let content = std::fs::read_to_string(&path_to_file).unwrap();
-
-    let tree = parser.parse(&content, None).unwrap();
-
-    let query = Query::new(language, "(attribute (identifier) @i (number) @v)").expect("unworkable query");
-
-    let root = tree.root_node();
-
-    println!("{}", root.to_sexp());
+    let query =
+        Query::new(language, "(attribute (identifier) @i (number) @v)").expect("unworkable query");
 
     let mut cursor = QueryCursor::new();
-    let matches = cursor.matches(&query, root, |node| {
-        "hi"
-    });
+    for entry in glob("**/*.tf").expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                let content = read_to_string(&path).unwrap();
 
-    for m in matches {
-        for capture in m.captures {
-            println!("{}", capture.node.utf8_text(&content.as_bytes()).unwrap());
+                let tree = parser.parse(&content, None).unwrap();
+
+                let matches = cursor.matches(&query, tree.root_node(), |_node| "");
+
+                for m in matches {
+                    for capture in m.captures {
+                        println!("{}", capture.node.utf8_text(&content.as_bytes()).unwrap());
+                    }
+                }
+            }
+            Err(e) => println!("{:?}", e),
         }
     }
 }
