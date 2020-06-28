b3e33fabb65f41c4785761c833c5059d24d06a08
Felipe Sere <felipesere@gmail.com>
2020-06-28 17:34:46 +0200

    Remove query subcommand

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,6 @@ fn main() {
         .version("0.1")
         .about("Checks terraform files for patterns")
         .subcommand(
-            App::new("query").arg(
-                Arg::new("query_file")
-                    .about("Runs a query and prints matches that bind to '@result'")
-                    .value_name("QUERY_FILE")
-                    .takes_value(true)
-                    .required(true),
-            ),
-        )
-        .subcommand(
             App::new("show")
                 .arg(
                     Arg::new("error_only")
@@ -47,15 +38,6 @@ fn main() {
     let parser = terraform::parser();
 
     match matches.subcommand() {
-        ("query", Some(query_matches)) => {
-            let file = query_matches.value_of("query_file").unwrap();
-            let content = read_to_string(file).unwrap();
-
-            let query = Query::new(parser.language().unwrap(), &content).expect("unworkable query");
-
-            run_query(parser, query)
-        }
-
         ("show", Some(show_matches)) => parse_all(parser, show_matches.is_present("error_only")),
         ("check", Some(check_matches)) => {
             run_check(parser, check_matches.value_of("rule_file").unwrap())
@@ -92,45 +74,6 @@ fn run_check(mut parser: Parser, rule_file: &str) {
                 println!("\n{}", path.to_str().unwrap());
             }
             err => println!("error: {:?}", err),
-        }
-    }
-}
-
-fn run_query(mut parser: Parser, query: Query) {
-    let mut cursor = QueryCursor::new();
-    for entry in glob("**/*.tf").expect("Failed to read glob pattern") {
-        match entry {
-            Ok(path) => {
-                let content = read_to_string(&path).unwrap();
-
-                let tree = parser.parse(&content, None).unwrap();
-
-                let text_callback = |n: Node| &content[n.byte_range()];
-
-                let mut matches = cursor
-                    .matches(&query, tree.root_node(), text_callback)
-                    .peekable();
-
-                if matches.peek().is_none() {
-                    continue;
-                }
-
-                println!("\n{}", path.to_str().unwrap());
-
-                for m in matches {
-                    for capture in m.captures {
-                        let name = &query.capture_names()[capture.index as usize];
-
-                        if name != "result" {
-                            continue;
-                        }
-                        println!("{}", capture.node.utf8_text(&content.as_bytes()).unwrap());
-                        println!("{}", capture.node.to_sexp());
-                        println!("");
-                    }
-                }
-            }
-            Err(e) => println!("{:?}", e),
         }
     }
 }
